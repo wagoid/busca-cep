1e6f102e5bd32b7970d204620f7e3caf4899c2d5
wagoid <w.rsantos@outlook.com>
2016-03-25 21:15:23 -0300

    Added the option to make a sync request.

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
 'use strict';
 
-var rp = require('request-promise');
+var requestPromise = require('request-promise');
+var syncRequest = require('sync-request');
 
 const CEP_SIZE = 8;
 const VIACEP_URI = 'https://viacep.com.br';
 
 function callViaCep(cep) {
-  var requestOptions = {
+  let requestOptions = {
     json: true,
     uri: `${VIACEP_URI}/ws/${cep}/json`
   };
 
-  return rp(requestOptions);
-}
-
-function makePromise (method) {
-  return new Promise(method);
+  return requestPromise(requestOptions);
 }
 
 function invalidCep (cep) {
-  return !cep || !isStringOrNumber(cep) || cep.length !== CEP_SIZE;
+  return !cep || !isStringOrNumber(cep) || cep.toString().length !== CEP_SIZE;
 }
 
 function isStringOrNumber (value) {
-  return typeof value === 'string' || value instanceof String;
+  return typeof value === 'string' || value instanceof String || !isNaN(value);
 }
 
-module.exports = function (cep) {
+module.exports = function (cep, sync) {
   if (cep && isNaN(cep)) {
     cep = cep.replace(/[-\s]/g, '');
   }
-
-  return makePromise((resolve, reject) => {
-    if (invalidCep(cep)) {
-      reject(`The CEP should be a number or string of size ${CEP_SIZE}. Please check your parameter.`);
-    } else {
-      callViaCep(cep)
-        .then(placeInfo => {
-          resolve(placeInfo);
-        })
-        .catch(err => {
-          reject(`There was an error searching for the cep. The error is ${err}`);
-        });
+  let ret;
+  
+  if (sync) {
+    try {
+      ret = syncRequest('GET', `${VIACEP_URI}/ws/${cep}/json`);
+      ret = JSON.parse(ret.getBody()) || {};
+    } catch(e) {
+      ret = {
+        statusCode: e.statusCode,
+        error: e.message
+      };
     }
-  });
+  } else {
+    ret = new Promise((resolve, reject) => {
+      if (invalidCep(cep)) {
+        reject(`The CEP should be a number or string of size ${CEP_SIZE}. Please check your parameter.`);
+      } else {
+        callViaCep(cep)
+          .then(placeInfo => {
+            resolve(placeInfo);
+          })
+          .catch(err => {
+            reject( {statusCode: err.statusCode, error: err.error});
+          });
+      }
+    });
+  }
+  
+  return ret;
+  
 };
