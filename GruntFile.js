dbd5e59d19dcdc8787135fa41958c8df9f88ccde
wagoid <w.rsantos@outlook.com>
2016-06-19 12:42:49 -0300

    Update travis and GruntFile.js

--- GruntFile.js
+++ GruntFile.js
@@ -11,11 +11,26 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
+    },
+    updatetravis: {
+      options: {
+        version: '>=4.0',
+        replacePreviousVersions: true,
+        numberOfVersionsPerMajorNumber: 3
+      }
+    },
+    bump: {
+      options: {
+        files: ['package.json'],
+        pushTo: 'origin'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-travis-node-updater');
+  grunt.loadNpmTasks('grunt-bump');
 
   grunt.registerTask('default', ['jshint']);
 
